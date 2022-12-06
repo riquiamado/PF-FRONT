8cfb60a8220168b7134822a3c41960c70405b611
VaniaML <vmlvcs@gmail.com>
2022-12-06 08:12:42 -0600

    Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default Footer () {
+
+return(
+    <div>
+        <ul>
+        <h4>Contacto</h4>
+        <li>+52 5512345678</li>
+        <li>Reforma 79, CDMX</li>
+        <li>fgdfdhf@gmail.com</li>
+        </ul>
+        
+        <ul>
+        <h4>Informacion</h4>
+        <li>Politica de Privacidad</li>
+        <li>Terminos y Condiciones</li>
+        </ul>
+
+        <ul>
+        <h4>Redes Sociales</h4> 
+        </ul>
+    </div>
+);
+}
--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function NavBar() {
   return (
     <div>NavBar</div>
