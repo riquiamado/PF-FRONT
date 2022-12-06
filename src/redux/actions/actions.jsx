6e5c9868ecf5a08eacb990761e9b3bb063560f1f
javixlive <javixlive@hotmail.com>
2022-12-05 19:05:27 -0600

    Corregi una ruta

--- src/redux/actions/actions.jsx
+++ src/redux/actions/actions.jsx
@@ -14,8 +14,8 @@ export   function getProviders (){
 
 export  function getProviderByName(name){
     return async function(dispatch){
-        const info = await axios.get(`http://localhost:3001/providers?name=${name}`)
-        
+        const info = await axios.get(`http://localhost:3001/provider?name=${name}`)
+        console.log(info.data)
         dispatch({type:GET_PROVIDER_BYNAME ,payload:info.data})
     }
 }
