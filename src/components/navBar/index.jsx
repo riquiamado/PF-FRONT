5f3799a89654a09560ab15ec1861c069206a010b
Andrea Barrera <andrea.barrera7@gmail.com>
2022-12-08 14:04:04 -0500

    Estilos de navbar y favicon

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
-import SearchBar from '../searchBar'
+import React from "react";
+import SearchBar from "../searchBar";
 // import Footer from '../footer'
-// import { Link } from 'react-router-dom'
-
-
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function NavBar() {
- 
- 
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(allServices());
+  };
+
   return (
     <div>
-    {/* <Footer/> */}
-    <SearchBar/>
-    
+      <nav class="navbar navbar-expand-md bg-light">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbar-toggler"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbar-toggler">
+            <a class="navbar-brand" href="#">
+              <img src="images/logo.png" width="50" alt="Website logo" />
+            </a>
+            <ul class="navbar-nav d-flex justify-content-center align-items-center">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  <Link to="/home" onClick={handleClick}>
+                    Home
+                  </Link>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <SearchBar />
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/components/searchBar/index.jsx
+++ src/components/searchBar/index.jsx
@@ -2,41 +2,50 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getServicesByName } from "../../redux/actions/actions";
 
-
 const SearchBar = () => {
   const [name, setName] = useState("");
-   const dispatch = useDispatch();
-  
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setName(e.target.value);
   };
 
   const handleSubmit = (el) => {
-    el.preventDefault()
-    if(!name){
-      return alert("Ingresa un nombre")
-    } else{
-      dispatch(getServicesByName(name))
-      setName("")
+    el.preventDefault();
+    if (!name) {
+      return alert("Ingresa un nombre");
+    } else {
+      dispatch(getServicesByName(name));
+      setName("");
     }
-   
   };
   return (
-  <div>
-    <div >
-        <input 
-        type="text" 
-        placeholder='Ingrese un name' 
-        value={name}  
-        onChange={(el => handleChange(el))}
-        className='inputSearch'
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Type a name"
+          value={name}
+          onChange={(el) => handleChange(el)}
+          className="inputSearch"
         />
-        
-        <button type='submit' className='boton' onClick={((el) => handleSubmit(el))} >Buscar Servicio</button>
+
+        {/* <button
+          type="submit"
+          className="boton"
+          onClick={(el) => handleSubmit(el)}
+        >
+          Search Service
+        </button> */}
+        <button
+          class="btn btn-outline-success"
+          type="submit"
+          onClick={(el) => handleSubmit(el)}
+        >
+          Search Service
+        </button>
       </div>
-    
-  </div>
+    </div>
   );
 };
 
