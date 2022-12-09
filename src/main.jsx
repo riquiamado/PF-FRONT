87ca74fec023f19397ef4c90858600a8035163ff
Andrea Barrera <andrea.barrera7@gmail.com>
2022-12-08 19:18:36 -0500

    Se realizaron los siguientes cambios:  - Se adiciono bulma y sass como dependencia  - El diseno del landing page  - El navbar para que sea realmente funcional  - Refactor de multiples funciones

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
-import store from "./redux/store"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import store from "./redux/store";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -15,4 +14,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
-)
+);
--- src/redux/reducer/index.jsx
+++ src/redux/reducer/index.jsx
@@ -1,100 +1,111 @@
-import { ADD_USERS, CLEAN, GET_USERS, GET_USERS_BY_NAME, GET_SERVICES, GET_SERVICES_DETAILS, GET_SERVICES_BY_NAME, ORDER_BY_NAME, ORDER_BY_SERVICES, ADD_SERVICES, FILTER_BY_SERVICES } from "../actions/components"
+import {
+  ADD_USERS,
+  CLEAN,
+  GET_USERS,
+  GET_USERS_BY_NAME,
+  GET_SERVICES,
+  GET_SERVICES_DETAILS,
+  GET_SERVICES_BY_NAME,
+  ORDER_BY_NAME,
+  ORDER_BY_SERVICES,
+  ADD_SERVICES,
+  FILTER_BY_SERVICES,
+} from "../actions/components";
 
 const initialState = {
-    allServices: [],
-    services: [],
-    users: [],
-    details: {}
-}
+  allServices: [],
+  services: [],
+  users: [],
+  details: {},
+};
 
 function rootReducer(state = initialState, action) {
-    switch (action.type) {
-        case GET_SERVICES:
-            return {
-                ...state,
-                services: action.payload,
-                allServices: action.payload
-            }
-        case GET_SERVICES_BY_NAME:
-            return {
-                ...state,
-                services: action.payload
-            }
+  switch (action.type) {
+    case GET_SERVICES:
+      return {
+        ...state,
+        services: action.payload,
+        allServices: action.payload,
+      };
+    case GET_SERVICES_BY_NAME:
+      return {
+        ...state,
+        services: action.payload,
+      };
 
-        case GET_USERS:
-            return {
-                ...state,
-                users: action.payload
-            }
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+      };
 
-        case GET_SERVICES_DETAILS:
-            return {
-                ...state,
-                details: action.payload
-            }
+    case GET_SERVICES_DETAILS:
+      return {
+        ...state,
+        details: action.payload,
+      };
 
-        case ADD_USERS:
-            return {
-                ...state,
-                users: action.payload
-            }
+    case ADD_USERS:
+      return {
+        ...state,
+        users: action.payload,
+      };
 
-        case ADD_SERVICES:
-            return {
-                ...state,
-                services: [...state.services, action.payload]
-            }
+    case ADD_SERVICES:
+      return {
+        ...state,
+        services: [...state.services, action.payload],
+      };
 
-        case ORDER_BY_SERVICES:
-            if (action.payload === "asc") {
-                return {
-                    ...state,
-                    services: [...state.services].sort(function (a, b) {
-                        if (a.name > b.name) {
-                            return 1;
-                        }
-                        if (b.name > a.name) {
-                            return -1;
-                        }
-                        return 0;
-                    }),
-                };
+    case ORDER_BY_SERVICES:
+      if (action.payload === "asc") {
+        return {
+          ...state,
+          services: [...state.services].sort(function (a, b) {
+            if (a.name > b.name) {
+              return 1;
             }
-            if (action.payload === "desc") {
-                return {
-                    ...state,
-                    services: [...state.services].sort(function (a, b) {
-                        if (a.name > b.name) {
-                            return -1;
-                        }
-                        if (b.name > a.name) {
-                            return 1;
-                        }
-                        return 0;
-                    }),
-                };
+            if (b.name > a.name) {
+              return -1;
             }
-            if (action.payload === "All") {
-                return {
-                    ...state,
-                    services: state.services,
-                };
-            } else {
-                return {
-                    ...state,
-                    services: state.allServices,
-                };
+            return 0;
+          }),
+        };
+      }
+      if (action.payload === "desc") {
+        return {
+          ...state,
+          services: [...state.services].sort(function (a, b) {
+            if (a.name > b.name) {
+              return -1;
             }
+            if (b.name > a.name) {
+              return 1;
+            }
+            return 0;
+          }),
+        };
+      }
+      if (action.payload === "All") {
+        return {
+          ...state,
+          services: state.services,
+        };
+      } else {
+        return {
+          ...state,
+          services: state.allServices,
+        };
+      }
 
-
-        case CLEAN:
-            return {
-                ...state,
-                details: {}
-            }
-        default:
-            return { ...state }
-    }
+    case CLEAN:
+      return {
+        ...state,
+        details: {},
+      };
+    default:
+      return { ...state };
+  }
 }
 
 export default rootReducer;
