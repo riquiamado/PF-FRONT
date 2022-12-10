d4f5153e001e53a3c26d21ca224c91ef9a231945
faka02 <facundoezequielalonso@gmail.com>
2022-12-10 19:39:53 -0300

    estilos cards paginado btn

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -7,11 +7,11 @@ function Cards({ _id, name, description, image }) {
     <div className="card">
           <img src={image} alt={image}/>
               <Link to={`services/${_id}`} style={{"textDecoration":"none","color":"black"}}>
-                <label htmlFor="">Services</label>
-                <h3>{name}</h3>{" "}
+                <label className="labelServices" htmlFor="">Service:</label>
+                <h3 className="h3cards">{name}</h3>{" "}
               </Link>
-          <label htmlFor="">Description</label>
-          <h3>{description}</h3>
+          <label className="labelDesc"htmlFor="">Description:</label>
+          <h4 className="h4Desc">{description}</h4>
     </div>
 
     // <div>
