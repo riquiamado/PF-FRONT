e94cd6822481296adbb42589d88d63f183a36a34
Erick140297 <erickmonterrubio67@outlook.com>
2022-12-10 20:49:03 -0600

    styles home

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,30 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./cards.css"
+import "./cards.css";
 
 function Cards({ _id, name, description, image }) {
   return (
-    <div className="card">
-          <img src={image} alt={image}/>
-              <Link to={`services/${_id}`} style={{"textDecoration":"none","color":"black"}}>
-                <label className="labelServices" htmlFor="">Service:</label>
-                <h3 className="h3cards">{name}</h3>{" "}
-              </Link>
-          <label className="labelDesc"htmlFor="">Description:</label>
-          <h4 className="h4Desc">{description}</h4>
-    </div>
-
-    // <div>
-    //   <div classNameName="cards">
-    //     <img src={image} alt={image} />
-    //     <Link to={`services/${_id}`}>
-    //       <label htmlFor="">Services</label>
-    //       <h3>{name}</h3>
-    //     </Link>
-    //     <label htmlFor="">description</label>
-    //     <h3>{description}</h3>
-    //   </div>
-    // </div>
+    <Link
+      to={`services/${_id}`}
+      style={{ textDecoration: "none", color: "black" }}
+    >
+      <div className="card">
+        <img src={image} alt={image} />
+        <div className="text">
+          <p>Service: </p>
+          <h3>{name}</h3>
+          <p>Description: </p>
+          <p>{description}</p>
+        </div>
+      </div>
+    </Link>
   );
 }
 
