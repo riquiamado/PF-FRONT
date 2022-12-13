5c3fb9f35a3cce462dabc7bd09e46b1c0a9760c6
javixlive <javixlive@hotmail.com>
2022-12-12 21:15:16 -0600

    Dashboard started

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./components/home";
 import NavBar from "./components/navBar";
 import "./App.css";
 import { checkLogin } from "./components/checkLogin";
+import Dashboard from "./components/dashboard/Dashboard";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route exact path="/users" component={CreateUser} />
       <Route exact path="/services" component={CreateServices} />
       <Route exact path="/checkLogin" component={checkLogin} />
+      <Route exact path="/dashboard" component={Dashboard} />
       <Route path="/services/:_id" component={CardDetails} />
     </div>
   );
