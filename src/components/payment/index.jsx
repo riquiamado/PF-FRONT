69d74429d0b065bb07a519f6edc95e3706611cc8
riquiamado <carlosamado221471@gmail.com>
2023-01-07 12:56:21 -0300

    estrellas

--- src/components/payment/index.jsx
+++ src/components/payment/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Payment = () => {
     return(
         <div>
             <h1>Thanks for your purchase!</h1>
+            <Link to={"/ratingForm"}><button>service qualification</button></Link>
         </div>
     )
 }
