7b30585090a67642d164c36f97ef96ff4e3945bd
Paula Ramírez <104044317+PaulaAn1@users.noreply.github.com>
2023-01-08 16:02:24 -0500

    estiloPayment

--- src/components/payment/index.jsx
+++ src/components/payment/index.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { login } from '../../redux/actions/actions';
+import "./payment.css";
 
 const Payment = () => {
     const loggedUser = window.localStorage.getItem("session")
+    const name = JSON.parse(window.localStorage.getItem("name"))
     const dispatch = useDispatch();
 
     useEffect( () => {
@@ -12,9 +14,26 @@ const Payment = () => {
     }, [])
     
     return(
-        <div>
-            <h1>Thanks for your purchase!</h1>
-            <Link to={"/ratingForm"}><button>service qualification</button></Link>
+        <div className="loginnds">
+            <div className="container-sm shadow p-3 mb-4 mt-4 bg-body rounded">
+                <h1 className="fs-2 fw-semibold pb-2 border-bottom border-muted">
+                    <i className="bi bi-emoji-laughing h4 pb-2 mb-4 me-2"></i>
+                    <i className="bi bi-box2-heart-fill h4 pb-2 mb-4 me-4 ms-4"></i>
+                    <i className="bi bi-emoji-laughing h4 pb-2 mb-4 me-2"></i>
+                </h1>
+                <div>
+                    <label className="text-muted mb-2 mt-4" htmlFor="">
+                        Hello {name.split(" ",1)}, thanks for your purchase!
+                    </label>
+                    <br />
+                    <label className="text-muted mb-2 mt-4" htmlFor="">
+                        <i className="bi bi-chat-left-text"><h3>Please, Don't forget to rate your service.</h3></i>
+                    </label>
+                </div>
+                <div className='mb-4 fs-5'>
+                    <Link to={"/ratingForm"} style={{textDecoration: 'none'}}>Clic here to Qualification</Link>
+                </div>
+            </div>
         </div>
     )
 }
