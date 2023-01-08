4792d47cbb944c039f981f9ae2862f1a3dc0cd83
Paula Ramírez <104044317+PaulaAn1@users.noreply.github.com>
2023-01-08 16:04:14 -0500

    estilospayment

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -110,7 +110,7 @@ function NavBar() {
             {session ? <b onClick={() => onLogout()}>Logout</b> : <b onClick={() => onLoging()}>Login</b>}
             </a>
           </div>
-          <i class="bi bi-cart4 fs-5"></i>
+          <i className="bi bi-cart4 fs-5"></i>
         </div>
       </div>
     </nav>
